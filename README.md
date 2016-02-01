ab7d99dc20cc8495f9aaf25bde51b3141bbc5253
Josh Johnston <josh@x-team.com>
2016-02-01 12:40:26 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# marki-journal
+
+POC for a marki-style journal.
+
+## General ideas
+
+- content is divided into notes (free-form) and topics (structured).
+- the goal is to make it easy & fast to take notes, and gradually refactor them into topics.
+
+## Requirenents
+
+- nodejs >= 4
---
+++ index.js
@@ -0,0 +1,69 @@
+'use strict'
+
+const async = require('async')
+const glob = require('glob')
+const path = require('path')
+
+const parseTopicFile = require('./lib/parse-topic-file')
+const parseNoteFile = require('./lib/parse-note-file')
+const render = require('./lib/render')
+
+const journalDir = path.join(__dirname, '..', '_data')
+const srcDir = path.join(journalDir, 'src')
+const srcTopicsDir = path.join(srcDir, 'topics')
+const srcNotesDir = path.join(srcDir, 'notes')
+
+const distDir = path.join(journalDir, 'dist')
+const distTopicsDir = path.join(distDir, 'topics')
+const distNotesDir = path.join(distDir, 'notes')
+
+const parseTasks = []
+
+// parse note files
+parseTasks.push(function (cb) {
+  glob(srcNotesDir + '/*.md', function (err, files) {
+    if (err) { return cb(err) }
+
+    const tasks = files.map(function (filename) {
+      return parseNoteFile.bind(null, filename)
+    })
+
+    async.parallel(tasks, cb)
+  })
+})
+
+// parse topic files
+parseTasks.push(function (cb) {
+  glob(srcTopicsDir + '/*.md', function (err, files) {
+    if (err) { return cb(err) }
+
+    const tasks = files.map(function (filename) {
+      return parseTopicFile.bind(null, filename)
+    })
+
+    async.parallel(tasks, cb)
+  })
+})
+
+// parse and render
+async.parallel(parseTasks, function (err, res) {
+  if (err) { throw err }
+
+  const data = {
+    notes: res[0],
+    topics: res[1]
+  }
+  const opts = {
+    distTopicsDir: distTopicsDir,
+    distNotesDir: distNotesDir
+  }
+
+  // TODO: validate
+
+  render(data, opts, function (err) {
+    if (err) { throw err }
+
+    conosle.log('ok')
+  })
+})
+
