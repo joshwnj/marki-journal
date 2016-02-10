31bcaa3420f6a6ef24af8317a11d04a5c4847d6a
Josh Johnston <josh@x-team.com>
2016-02-10 12:24:14 +1100

    refactor some things to make them easier to test

--- build.js
+++ build.js
@@ -3,40 +3,39 @@
 const async = require('async')
 const glob = require('glob')
 
+const loadAndProcessAll = require('./lib/load-and-process-all')
 const parseTopicFile = require('./lib/parse-topic-file')
 const parseNoteFile = require('./lib/parse-note-file')
 const render = require('./lib/render')
 const validate = require('./lib/validate')
 
+function loadNotes (dir, cb) {
+  glob(dir + '/*.md', (err, filenames) => {
+    if (err) { return cb(err) }
+
+    console.log('notes: %d', filenames.length)
+    loadAndProcessAll(filenames, parseNoteFile, cb)
+  })
+}
+
+function loadTopics (dir, cb) {
+  glob(dir + '/*.md', (err, filenames) => {
+    if (err) { return cb(err) }
+
+    console.log('topics: %d', filenames.length)
+    loadAndProcessAll(filenames, parseTopicFile, cb)
+  })
+}
+
 module.exports = function build (opts, cb) {
-  const parseTasks = []
+  const tasks = []
 
-  // parse note files
-  parseTasks.push(cb => {
-    glob(opts.srcNotesDir + '/*.md', (err, files) => {
-      if (err) { return cb(err) }
-
-      console.log('notes: %d', files.length)
-      const tasks = files.map(filename => parseNoteFile.bind(null, filename))
-
-      async.parallel(tasks, cb)
-    })
-  })
-
-  // parse topic files
-  parseTasks.push(cb => {
-    glob(opts.srcTopicsDir + '/*.md', (err, files) => {
-      if (err) { return cb(err) }
-
-      console.log('topics: %d', files.length)
-      const tasks = files.map(filename => parseTopicFile.bind(null, filename))
-
-      async.parallel(tasks, cb)
-    })
-  })
+  // load src files
+  tasks.push(loadNotes.bind(null, opts.srcNotesDir))
+  tasks.push(loadTopics.bind(null, opts.srcTopicsDir))
 
   // parse, validate, render
-  async.parallel(parseTasks, (err, res) => {
+  async.parallel(tasks, (err, res) => {
     if (err) { return cb(err) }
 
     const data = {
